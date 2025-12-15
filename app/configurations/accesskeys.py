1303186d2d0e6d8d1e4c7f1401e25bf39ede74a7
Eddy <edifonemmanuel14@gmail.com>
2025-12-15 13:46:20 +0100

    updated config

--- app/configurations/accesskeys.py
+++ app/configurations/accesskeys.py
@@ -1,5 +1,7 @@
+# Import libraries
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+# Create class
 class AccessKeysConfig(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env",
                                       env_file_encoding="utf-8")
